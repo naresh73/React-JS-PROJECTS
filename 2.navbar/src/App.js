30a4ca02ddfe33c945f3c12f5268890c0f0cb8de
nareshsharma0318@gmail.com <nareshsharma0318@gmail.com>
2022-07-15 07:46:21 +0530

    edited

--- 2.navbar/src/App.js
+++ 2.navbar/src/App.js
@@ -4,17 +4,12 @@ import Foot from './components/Footer/footer';
 import FormFill from './components/Form/form';
 import ResponsiveAppBar from './components/Navbar/navbar';
 
-// import Slide from './navbar';
-
 
 function App() {
-
+      
   return (
     <div>
-      {/* <Slide/> */}
-      <header>
       <ResponsiveAppBar />
-      </header>
       <FormFill />
       <Foot />
     </div>
